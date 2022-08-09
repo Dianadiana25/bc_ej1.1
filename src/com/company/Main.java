e552103e5fa5fcaa447de3cb4c954cb2e7026b3e
Diana <d.g.c.2513@gmail.com>
2022-08-08 23:13:48 -0300

    commit inicial

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+
+    {   int igual = 0;
+        igual = mas( 7, 9, 5);
+        System.out.println(igual);
+    }
+
+    public static int mas(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
+
+
